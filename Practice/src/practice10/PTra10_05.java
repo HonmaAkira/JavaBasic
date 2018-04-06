a403197d1a80fd7a22778acc9169800a0e56327e
HonmaAkira <akiranran28@gmail.com>
2018-04-06 14:47:37 +0900

    PTra10_05まで終了

--- Practice/src/practice10/PTra10_05.java
+++ Practice/src/practice10/PTra10_05.java
@@ -1,10 +1,4 @@
 package practice10;
-/*
- * PTra10_05.java
- *   作成	LIKEIT	2017
- *------------------------------------------------------------
- * Copyright(c) Rhizome Inc. All Rights Reserved.
- */
 
 public class PTra10_05 {
 
@@ -23,20 +17,22 @@ public class PTra10_05 {
 	 * 			ガソリンが負の数になった場合（もう進めない）は-1を返します。
 	 */
 
+
 	public static void main(String[] args) {
 
 		// Carクラスを作成後に着手してください
 		// ★ Car型の変数carを宣言し、Carクラスのインスタンスを代入してください
-
+		Car car;
+		car = new Car();
 
 		// ★ 変数carに格納されているインスタンスのserialNoフィールドに、10000を代入してください
-
+		car.serialNo = 10000;
 
 		// ★ 変数carに格納されているインスタンスのcolorフィールドに、"Red"を代入してください
-
+		car.color = "Red";
 
 		// ★ 変数carに格納されているインスタンスのgasolineフィールドに、50を代入してください
-
+		car.gasoline = 50;
 
 		// 目的地までの距離
 		final int distance = 300;
@@ -47,6 +43,22 @@ public class PTra10_05 {
 		 * ★ 目的地についた時点で「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください
 		 * ※n：runメソッドを実行した回数, xは残りのガソリンの数です
 		 */
+		int sum = 0;
+		int count = 0;
+		while(true) {
+			int i = car.run();
+			if(car.gasoline == 0) {
+				System.out.println("目的地に到達できませんでした");
+				break;
+			}
 
+			sum += i;
+			System.out.println(sum);
+			count++;
+			 if(sum>=300) {
+				System.out.println("目的地にまで"+count+"時間かかりました。残りのガソリンは、"+(car.gasoline)+"リットルです");
+				break;
+			}
+		}
 	}
 }
