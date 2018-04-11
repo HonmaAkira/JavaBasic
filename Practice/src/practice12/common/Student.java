7bbd86ea2e6d09024396a3f07a0171acf294be09
HonmaAkira <akiranran28@gmail.com>
2018-04-11 10:36:18 +0900

    PTra12_01まで終了

--- Practice/src/practice12/common/Student.java
+++ Practice/src/practice12/common/Student.java
@@ -13,20 +13,73 @@ package practice12.common;
  */
 public class Student {
 	/** 受講生ID */
-	public int studentId;
+	private int studentId;
 
+	public int getstudentId() {
+		return this.studentId;
+	}
+	public void setstudentId(int ID) {
+		this.studentId = ID;
+	}
+	
+	
+	
 	/** 受講生氏名 */
-	public String studentName;
+	private String studentName;
 
+	public String getstudentName() {
+		return this.studentName;
+	}
+	public void setstudentName(String name) {
+		this.studentName = name;
+	}
+	
+	
+	
 	/** 会社名 */
-	public String companyName;
+	private String companyName;
 
+	public String getcompanyName() {
+		return this.companyName;
+	}
+	public void setcompanyName(String com) {
+		this.companyName = com;
+	}
+	
+	
+	
 	/** 教室名 */
-	public String className;
+	private String className;
 
+	public String getclassName(){
+		return this.className;
+	}
+	public void setclassName(String c) {
+		this.className = c;
+	}
+	
+	
+	
 	/** メールアドレス */
-	public String mail;
+	private String mail;
 
+	public String getmail() {
+		return this.mail;
+	}
+	public void setmail(String mail) {
+		this.mail = mail;
+	}
+	
+	
+	
 	/** パスワード */
-	public String password;
+	private String password;
+	
+	public String getpassword() {
+		return this.password;
+	}
+	
+	public void setpassword(String p) {
+		this.password =p;
+	}
 }
