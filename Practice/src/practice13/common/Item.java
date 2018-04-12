9b57668438156a8fb0bdcdd14f115d6c8559d492
HonmaAkira <akiranran28@gmail.com>
2018-04-12 15:23:34 +0900

    PTra13_05まで終了

---
+++ Practice/src/practice13/common/Item.java
@@ -0,0 +1,25 @@
+package practice13.common;
+
+
+public class Item{
+	private String name;
+	private int aditionalDamage;
+
+		public String getname() {
+			return this.name;
+		}
+		public void setname(String name) {
+			this.name= name;
+	}
+
+		public int getaditionalDamage() {
+			return this.aditionalDamage;
+		}
+		public void setaditionalDamage(int aditional) {
+			this.aditionalDamage = aditional;
+		}
+
+		Item(String name,int aditional){
+
+		}
+}
