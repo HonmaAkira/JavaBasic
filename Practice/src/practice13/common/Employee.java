ea474dee747c6436f90363c73c1c09a6f8ee1a36
HonmaAkira <akiranran28@gmail.com>
2018-04-12 18:03:07 +0900

    Employeeクラス作成まで終了

---
+++ Practice/src/practice13/common/Employee.java
@@ -0,0 +1,22 @@
+package practice13.common;
+
+public class Employee extends Person{
+	private String departmentNm;
+	private int departmantCnt;
+	public String getDepartmentNm() {
+		return departmentNm;
+	}
+	public void setDepartmentNm(String departmentNm) {
+		this.departmentNm = departmentNm;
+	}
+	public int getDepartmantCnt() {
+		return departmantCnt;
+	}
+	public void setDepartmantCnt(int departmantCnt) {
+		this.departmantCnt = departmantCnt;
+	}
+
+
+}
+
+
